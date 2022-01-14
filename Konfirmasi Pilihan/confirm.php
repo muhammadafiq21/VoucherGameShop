0b61137e48cf3d053156e7721bae1cd6f6d7241a
Aran27 <ashabrivanyayardan@gmail.com>
2022-01-14 07:25:30 +0700

    konfirmasi pilihan

---
+++ Konfirmasi Pilihan/confirm.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Konfirmasi Pembayaran</title>
+    <link rel="stylesheet" href="/hehe/VoucherGameShop/Code/Homepage.css">
+    <link rel="stylesheet" href="confirm.css">
+    
+</head>
+
+<body>
+    <!-- Navbar -->
+    <div class="navbar">
+        <div class="logo">
+            <img src="TokoTokoTok Logo-wide.png" class="gamepad" alt="logo" width="105px">
+        </div>
+        <div class="bacaan">
+            <p><font size="4">SUDAH SIAP MENGHABISKAN DOMPET MAMAH?</font></p>
+        </div>
+        <a href="" class="contact"><button>Contact &#127939;</button></a>
+    </div>
+
+    <div class="grid-container">
+        <div class="grid-item" id="ex"></div>
+        <div class="grid-item">
+            <h3 style="background-color: #6DB270; border-radius: 20px 20px 0px 0px; padding: 20px; font-family: Montserrat, sans-serif;">Detail Pemesanan</h3><br>
+            <div style="padding: 25px;">
+                <p style="text-align: left; font-size: 18px; cursor:auto; color:black">Mohon konfirmasi ID dan pilihan anda sudah benar.<br><br><br>
+                    ID : <br><br>
+                    Harga :<br><br>
+                    Bayar dengan :<br><br>
+                    Total Pembayaran :
+                </p>
+                <div style="text-align:right; margin-top: 15vh;">
+                    <button style="color: black; margin-right:10px">Batalkan</button>
+                    <button style="color: black;">Konfirm</button>
+                </div>            
+            </div>
+        </div>
+        <div class="grid-item" id="ex"></div>
+    </div>    
+</body>
+</html>
